dff573396a54c456f35e81a109cc8965bcb62a35
Jason Cha <jasonyhcha@gmail.com>
2017-03-07 16:55:33 -0800

    Initial commit.

---
+++ js/index.js
@@ -0,0 +1,34 @@
+$(document).ready(() => {
+  $(".searchForm").submit(e => {
+    e.preventDefault();
+    let text = e.target.searchbar.value;
+      $(".results-container").empty();
+      animate();
+      $.get("https://search-engine-tornado.herokuapp.com/search/api/"+text, function(data){
+        fillResults(data);
+      });
+  });
+});
+
+function fillResults(data){
+  let result = "";
+  for(let i=0; i<data.content.length; i++){
+    let url = data.content[i].url;
+    if(url.length > 70)
+      url = url.substring(0,70) + "...";
+    result += "<a href=" + data.content[i].url + "><span class='title'>" + data.content[i].title + "</span></a>\n";
+    result += "<p><span class='link'>" + url + "</span><br>";
+    result += data.content[i].description + "</p>";
+  }
+  $(".results-container").append(result);
+}
+
+
+function animate(){
+  $('.main-title').animate({
+    marginTop:'2%'
+  });
+  $('.search-container').animate({
+    marginTop:'20px'
+  });
+}
